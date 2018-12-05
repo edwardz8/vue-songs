b978b823774f5b7d401e115b4515bae5836256f4
edwardz8 <tolklorist@gmail.com>
2018-12-04 20:03:59 -0600

    add register logic

---
+++ client/src/components/Register.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>Register</h2>
+
+    <input type="email" v-model="email" name="email" placeholder="email">
+
+    <input type="password" v-model="password" name="password" placeholder="password">
+
+    <br>
+
+    <button @click="register">Sign Up</button>
+
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  watch: {
+     email(value) {
+      console.warn('email has changed', value)
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(this.data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+a {
+  color: #35495E;
+}
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -4,14 +4,14 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-/* app.use(require('connect').bodyParser()); */
-
-app.get("/status", function(req, res) {
-    return res.send('Hello World!');
+app.post('/register', function(req, res) {
+    res.send({
+      message: `Hello ${req.body.email}! You have been registered. Thanks for signing up.`
+    })
   });
 
 app.listen(process.env.PORT || 8081)
